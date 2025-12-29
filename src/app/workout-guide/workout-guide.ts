5fd93955ec9cb71b557b073bfd66f5beaaee3345
Pavithra2010 <pavithra.2205107@srec.ac.in>
2025-12-29 12:08:43 +0530

    Updated workout guide UI

--- src/app/workout-guide/workout-guide.ts
+++ src/app/workout-guide/workout-guide.ts
@@ -9,21 +9,16 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./workout-guide.css']
 })
 export class WorkoutGuideComponent {
-
   userName: string = '';
   weight: number = 0;
-
   goal: 'weight_loss' | 'muscle_gain' | 'maintenance' | null = null;
-
   ngOnInit() {
     this.userName = localStorage.getItem('userName') || 'Guest User';
     this.weight = Number(localStorage.getItem('weight')) || 0;
   }
-
   selectGoal(goal: 'weight_loss' | 'muscle_gain' | 'maintenance') {
     this.goal = goal;
   }
-
   resetGoal() {
     this.goal = null;
   }
